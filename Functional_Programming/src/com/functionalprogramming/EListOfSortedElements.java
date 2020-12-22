bc3bf665d0e75cbf280e72b46711c1cd0a834f5f
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2020-12-22 18:23:05 +0530

    [Functional programming in Java]feature Implementation of sorted elements

--- Functional_Programming/src/com/functionalprogramming/EListOfSortedElements.java
+++ Functional_Programming/src/com/functionalprogramming/EListOfSortedElements.java
@@ -1,4 +1,13 @@
 package com.functionalprogramming;
 
+import java.util.List;
+
 public class EListOfSortedElements {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        System.out.println("-----List of Sorted elements using sorted function-----");
+        list.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
 }
